b2684c3b02f7d44f34b07d490d59b13b393a9d1e
ehu <fehuehuf@gmail.com>
2017-12-31 00:26:35 +0900

    first commit

---
+++ app/src/main/java/com/example/ehu/myschedular/MySchesularApplication.java
@@ -0,0 +1,18 @@
+package com.example.ehu.myschedular;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmCollection;
+import io.realm.RealmConfiguration;
+
+
+public class MySchesularApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+        RealmConfiguration realmConfig=new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(realmConfig);
+    }
+}
