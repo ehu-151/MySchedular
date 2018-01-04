56cca6e93e7c543d91afdb2efec736a5f29e5a6c
ehu <fehuehuf@gmail.com>
2018-01-04 23:29:23 +0900

    スケジュールの新規作成機能の追加

--- app/src/main/java/com/example/ehu/myschedular/MainActivity.java
+++ app/src/main/java/com/example/ehu/myschedular/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ehu.myschedular;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Realm mRealm;
     private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +30,15 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this, ScheduleEditActivity.class));
             }
         });
         //Realmのインスタンス取得：準備
-        mRealm= Realm.getDefaultInstance();
+        mRealm = Realm.getDefaultInstance();
         //
-        mListView =(ListView)findViewById(R.id.listView);
-        RealmResults<Schedule>schedules=mRealm.where(Schedule.class).findAll();
-        ScheduleAdapter adapter=new ScheduleAdapter(schedules);
+        mListView = (ListView) findViewById(R.id.listView);
+        RealmResults<Schedule> schedules = mRealm.where(Schedule.class).findAll();
+        ScheduleAdapter adapter = new ScheduleAdapter(schedules);
         mListView.setAdapter(adapter);
     }
 
